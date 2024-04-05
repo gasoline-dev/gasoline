5400aa3230b48e8a557789e0aa13aa75421d216a
Shane <95544653+shaneuuid@users.noreply.github.com>
2024-04-05 13:35:22 -0400

    Update templates with resource setters

--- templates/cloudflare-kv/src/index.ts
+++ templates/cloudflare-kv/src/index.ts
@@ -1,3 +1,6 @@
-export const config = {
+import { setCloudflareKv } from "@gasoline-dev/resources";
+
+export const coreBaseKv = setCloudflareKv({
+	id: "core:base:cloudflare-kv:v1:12345",
 	namespace: "",
-};
+} as const);
--- templates/cloudflare-worker-api-hono/src/index.ts
+++ templates/cloudflare-worker-api-hono/src/index.ts
@@ -1,5 +1,13 @@
 import { Hono } from "hono";
-const app = new Hono();
+import { setCloudflareWorker } from "@gasoline-dev/resources";
+
+export const config = setCloudflareWorker({
+	id: "core:base:cf-worker:api:v1:12345",
+} as const);
+
+type Bindings = {};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/", (c) => c.text("Hello, World!"));
 
