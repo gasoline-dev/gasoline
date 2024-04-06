cd62b42d6d31bc15bad846fbdf820b2f08629c0c
Shane <95544653+shaneuuid@users.noreply.github.com>
2024-04-06 10:50:25 -0400

    Add cf pages & service bindings

--- packages/resources/src/index.resources.ts
+++ packages/resources/src/index.resources.ts
@@ -1,9 +1,15 @@
-import { KVNamespace } from "@cloudflare/workers-types";
+import { Fetcher, KVNamespace } from "@cloudflare/workers-types";
 
 export type KvBindings<T extends ReadonlyArray<{ readonly binding: string }>> =
 	{
 		[P in T[number]["binding"]]: KVNamespace;
 	};
+
+export type ServiceBindings<
+	T extends ReadonlyArray<{ readonly binding: string }>,
+> = {
+	[P in T[number]["binding"]]: Fetcher;
+};
 
 type CloudflareKv = {
 	id: string;
@@ -11,6 +17,18 @@ type CloudflareKv = {
 };
 
 export function setCloudflareKv<T extends CloudflareKv>(resource: T): T {
+	return resource;
+}
+
+type CloudflarePages = {
+	id: string;
+	name: string;
+	services: Array<{
+		binding: string;
+	}>;
+};
+
+export function setCloudflarePages<T extends CloudflarePages>(resource: T): T {
 	return resource;
 }
 
